d71f13b1c784941af39c2906a5d5e28c2f534eba
Ryan Lok <rlok.pc@gmail.com>
2025-07-31 14:23:59 -0700

    completed 42

---
+++ 42.js
@@ -0,0 +1,36 @@
+//42. Trapping Rain Water
+/**
+Kind of overthinked the solution for this one initially. 
+- Solutions are typically simpler than expected, need to start thinking with simpler solutions first
+
+ */
+
+function trap(height) {
+
+    const tallestLeft = Array(height.length).fill(0)
+    const tallestRight = Array(height.length).fill(0)
+
+    let maxLeft = 0
+    let maxRight = 0
+    let output = 0
+
+    for (let i = 0; i < height.length; i++) {
+        tallestLeft[i] = maxLeft
+        maxLeft = Math.max(height[i], maxLeft)
+    }
+
+    for (let i = height.length - 1; i >= 0; i--) {
+        tallestRight[i] = maxRight
+        maxRight = Math.max(height[i], maxRight)
+    }
+
+    for (let i = 0; i < height.length; i++) {
+        let minBoundary = Math.min(tallestLeft[i], tallestRight[i])
+        if (minBoundary > height[i]) {
+            output += minBoundary - height[i]
+        }
+    }
+
+    return output
+}
+
